684428562c6c4baed2c133562951aca55bd26b74
Jahid Hasan <jahid.dev39@gmail.com>
2019-10-31 02:45:43 +0600

    fix mail

--- dist/contact.php
+++ dist/contact.php
@@ -1,19 +1,45 @@
 <?php
-
-if($_POST["submit"]) {
-    $recipient="scooper@procura.ca,park@procura.ca,customerservice@procura.ca,devjahid@gmail.com";
-    $subject="Query: parkprocura";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    $retval = mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-    if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
+    
+    // Submitted form data
+    $name   = $_POST['name'];
+    $email  = $_POST['email'];
+    $message= $_POST['message'];
+    
+    /*
+     * Send email to admin
+     */
+    $to     = 'scooper@procura.ca,customerservice@procura.ca,park@procura.ca';
+    $subject= 'Contact Request Submitted';
+    
+    $htmlContent = '
+    <h4>Contact request has submitted at CodexWorld, details are given below.</h4>
+    <table cellspacing="0" style="width: 300px; height: 200px;">
+        <tr>
+            <th>Name:</th><td>'.$name.'</td>
+        </tr>
+        <tr style="background-color: #e0e0e0;">
+            <th>Email:</th><td>'.$email.'</td>
+        </tr>
+        <tr>
+            <th>Message:</th><td>'.$message.'</td>
+        </tr>
+    </table>';
+    
+    // Set content-type header for sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    
+    // Additional headers
+    //$headers .= 'From: CodexWorld<sender@example.com>' . "\r\n";
+    
+    // Send email
+    if(mail($to,$subject,$htmlContent,$headers)){
+        $status = 'ok';
+    }else{
+        $status = 'err';
+    }
+    
+    // Output status
+    echo $status;die;
 }
-
-?>
